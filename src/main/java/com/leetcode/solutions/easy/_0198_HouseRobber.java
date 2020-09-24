0425f3de4899cd5a7befaba869d92ce4a3ac5efb
iamminji <minji.dev@gmail.com>
2020-09-24 10:07:33 +0900

    [20200924] Solve easy problems

---
+++ src/main/java/com/leetcode/solutions/easy/_0198_HouseRobber.java
@@ -0,0 +1,30 @@
+package com.leetcode.solutions.easy;
+
+
+/**
+ * Try 2
+ *  nums input 값의 범위를 제대로 확인 하지 않았다.
+ */
+public class _0198_HouseRobber {
+    public int rob(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        int result = dp[1];
+
+        for (int i = 2; i< nums.length; i++) {
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+            result = Math.max(dp[i], result);
+        }
+        return result;
+    }
+}
